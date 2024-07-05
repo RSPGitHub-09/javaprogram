97acb82faa8b2d8c0807adf4947abcbf585647b4
RSPGitHub-09 <suryaprakashrupanshu27@gmail.com>
2024-07-05 22:57:18 +0530

    pushed

---
+++ Day01.java
@@ -0,0 +1,14 @@
+package Automation;
+
+public class Day01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Rupanshu");
+		System.out.println("Software Engineer");
+		System.out.println("Noida");
+		System.out.println("Work From Home");
+
+	}
+
+}
